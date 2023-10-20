addeb09f2360cbcb97b6694f74f2d0935338b38e
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-10-20 18:45:14 +0100

    tasks

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' Task 3'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    default_text = text or 'is cool'
+    result = f"Python {default_text}"
+    return result
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def int():
+    return '{n} + is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
