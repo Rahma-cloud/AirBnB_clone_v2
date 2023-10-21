a6c0b5be323703b09c15d9125a0e6aa2c65188ca
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-10-21 11:27:02 +0100

    tasks

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' Task 5'''
+''' Task 6'''
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -35,6 +35,14 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    odd_or_even = "even" if n % 2 == 0 else "odd"
+    return render_template(
+        '6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even
+        )
 
 
 if __name__ == '__main__':
