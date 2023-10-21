f19e992cd8c7a072c2d6cc05c7d0564426e5e14b
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-10-21 13:00:23 +0100

    tasks

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Task 7"""
+
+from flask import Flask, render_template 
+from models import storage 
+from models.state import State 
+  
+app = Flask(__name__, template_folder="templates") 
+  
+  
+@app.route('/states_list', strict_slashes=False) 
+def states_list(): 
+     """Display a list of states with their IDs and names""" 
+     states = storage.all(State).values() 
+     return render_template('7-states_list.html', states=states) 
+  
+  
+@app.teardown_appcontext 
+def teardown(exception): 
+     """Close the current SQLAlchemy Session""" 
+     storage.close() 
+  
+  
+if __name__ == '_main_': 
+     app.run(host='0.0.0.0', port=5000)
